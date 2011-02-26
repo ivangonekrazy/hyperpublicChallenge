e11d02e6a510712ef18155b839c64daf39f7f676
Ivan Tam <ivangonekrazy@gmail.com>
2011-02-25 22:57:02 -0800

    yay, working version producing the answer!

--- challenge2.py
+++ challenge2.py
@@ -5,28 +5,49 @@
     Ivan Tam, ivan@hipnik.net.
 """
 
-import pprint
-pp = pprint.PrettyPrinter(indent=2)
+def readInput(filename="challenge2input.txt"):
+  """ read the input file and return a list of
+      readlines
+  """
+  lines = []
+  f = None
+  try:
+      f = open(filename)
+      lines = f.readlines()
+  except IOError, e:
+      print "IOError on opening: %s" % str(e)
+  finally:
+      if f: f.close()
+  return lines
 
-lines = []
-f = None
-try:
-    f = open("challenge2input.txt")
-    lines = f.readlines()
-except IOError, e:
-    print "Fail: %s" % str(e)
-finally:
-    if f: f.close()
+def inviteTable(lines):
+  """ takes a list of the readlines and
+      builds the table of who invited who
+  """
+  users = {}
+  for i in xrange( len(lines) ):
+      users[i] =[] 
 
-influences = {}
+  for i,line in enumerate(lines):
+      for j,user in enumerate(line):
+          if user == 'X':
+              users[i].append(j)
+  return users
 
-users = {}
-for i in xrange( len(lines) ):
-    users[i] =[] 
+def influence( user, debug=False ):
+  """ given a user, return her influence
+  """
+  if users[user] == []:
+    if debug: print "user %s has invited no one" % user
+    return 0 
+  else:
+    i = len( users[user] ) 
+    if debug: print "user %s has invited %s" % (user, ",".join([ str(x) for x in users[user]] ) )
+    for u in users[user]:
+      i += influence( u, debug )
+  return i 
 
-for i,line in enumerate(lines):
-    for j,user in enumerate(line):
-        if user == 'X':
-            users[i].append(j)
+users = inviteTable( readInput() )
 
-pp.pprint(users)
+# yay, the top 3 influencers!
+print sorted([ j for j,k in sorted([ (influence(x), x) for x in users.keys() ])[-3:] ])
